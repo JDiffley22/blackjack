927ff5f59e78c8dd2f93e62b723466340780f762
James Diffley <diffs1991@googlemail.com>
2024-05-22 13:14:58 +0100

    Card total added and number display added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageAsk = document.getElementById("message-ask")
-console.log(messageAsk)
+let messageSum = document.getElementById("message-sum")
+
+let gameCards = document.getElementById("cards")
 
 function startGame() {
+
+    gameCards.textContent = "Cards: " + firstCard + " " + secondCard
+    messageSum.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
