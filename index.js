785aad46d41d8d5e8fcc6a92a9fac24173a1f590
James Diffley <diffs1991@googlemail.com>
2024-05-23 16:09:23 +0100

    Random card can be drawn from deck, new card function with parameters added

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-let firstCard = 10
-let secondCard = 9
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageAsk = document.getElementById("message-ask")
 let messageSum = document.getElementById("message-sum")
-
 let gameCards = document.getElementById("cards")
 
-function startGame() {
+console.log(cards)
 
-    gameCards.textContent = "Cards: " + firstCard + " " + secondCard
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    gameCards.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        gameCards.textContent += cards[i] + " "
+    }
+
     messageSum.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -25,3 +48,13 @@ function startGame() {
     messageAsk.textContent = message
 }
 
+function newCard() {
+
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum+=card
+        cards.push(card)
+        renderGame()
+    }
+
+}
